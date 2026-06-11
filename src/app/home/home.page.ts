563160485d3c25bb9e8a94d8aea8aaacbd36840a
Natalia Ortiz Martinez <deyaneira.ortiz.martinez@est.una.ac.cr>
2026-06-11 17:14:31 -0600

    Code for the correct implementation of Mood Tracker Express

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonButton,
+    IonCard,
+    IonCardContent,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  emojiActual = '🤔';
+  mensajeActual = '¿Cómo te sientes hoy? Selecciona una opción abajo.';
+  colorFondo = 'light';
+
+  cambiarEstado(nuevoEmoji: string, nuevoMensaje: string, nuevoColor: string) {
+    this.emojiActual = nuevoEmoji;
+    this.mensajeActual = nuevoMensaje;
+    this.colorFondo = nuevoColor;
+  }
 }
